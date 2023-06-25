c58215b7163b108c19151fcdba5181ffc2b9bc74
Megersa Muleta <megibiratu@icloud.com>
2023-06-25 22:51:02 +0300

    add files

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,7 @@ app.use(cors({
              //access-control-allow-credentials:true
     optionSuccessStatus:200
 }));
- app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "https://afro-web.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+  
 
 app.use(express.json());
 app.use(cookieParser());
