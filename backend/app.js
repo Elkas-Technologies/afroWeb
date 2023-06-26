6cbd78bedefee41f975332f22154bb1d9f317fe9
Megersa Muleta <megibiratu@icloud.com>
2023-06-26 11:59:59 +0300

    enter changing vercel and cyclic apis

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://afro-web.vercel.app',
   credentials: true,
      
 }));
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8080/api/v2";
+export const server = "http://modern-cap-lion.cyclic.app/api/v2";
 
-export const backend_url = "http://localhost:8080/";
+export const backend_url = "http://modern-cap-lion.cyclic.app/";
