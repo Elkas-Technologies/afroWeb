2630d6a79ff273a8a1b5cab7f0ab759a9ff55779
Megersa Muleta <megibiratu@icloud.com>
2023-06-26 11:23:12 +0300

    client side api is not connecting with the backend so lets check it with local host

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,8 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://afro-web.vercel.app',
-  header: 'Access-Control-Allow-Origin: *',
-  header: 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE',
-  header: 'Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With',
+  origin: 'http://localhost:3000',
+   
   credentials: true,
      
 }));
