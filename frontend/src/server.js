c1fcd3251935c41982bc7905b823e6cfd0afd711
Megersa Muleta <megibiratu@icloud.com>
2023-06-26 13:23:30 +0300

    getting cyclic api url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://modern-cap-lion.cyclic.app/api/v2";
+export const server = "https://adorable-hospital-gown-seal.cyclic.app/api/v2";
 
-export const backend_url = "https://modern-cap-lion.cyclic.app/";
+export const backend_url = "https://adorable-hospital-gown-seal.cyclic.app/";
