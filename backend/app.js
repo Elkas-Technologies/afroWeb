14c085e5fc09812f28d47fbc72b39fa7ce67efb6
Megersa Muleta <megibiratu@icloud.com>
2023-06-27 19:04:18 +0300

    res values

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ app.use(cors({
 }));
   app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://afro-web-megersam.vercel.app");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
  
