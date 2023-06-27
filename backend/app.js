37e5ef95df6fb3345517f304eb55275474d28964
Megersa Muleta <megibiratu@icloud.com>
2023-06-27 19:11:29 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
 app.use("/api/v2/message", message);
 app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
+app.use("api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
