3e35fbb6d63ad8b303c5001dc850abb446f7ae93
Megersa Muleta <megibiratu@icloud.com>
2023-07-15 12:06:44 +0300

    frontend api call changes

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -65,7 +65,7 @@ const ProductCard = ({ data,isEvent }) => {
     <>
       <div className="w-full h-[370px] bg-white rounded-lg shadow-sm p-3 relative cursor-pointer">
         <div className="flex justify-end"></div>
-        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
+        <Link to={`${isEvent === true ? `/program/${data._id}?isEvent=true` : `/program/${data._id}`}`}>
           <img
             src={`${backend_url}${data.images && data.images[0]}`}
             alt=""
@@ -75,7 +75,7 @@ const ProductCard = ({ data,isEvent }) => {
         <Link to={`/shop/preview/${data?.shop._id}`}>
           <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
         </Link>
-        <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
+        <Link to={`${isEvent === true ? `/program/${data._id}?isEvent=true` : `/program/${data._id}`}`}>
           <h4 className="pb-3 font-[500]">
             {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
           </h4>
